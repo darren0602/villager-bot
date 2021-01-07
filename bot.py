a672b6a76bca7d48aa9daac46e22aec7004e16fc
pongloongyeat <pongloongyeat@gmail.com>
2021-01-07 21:48:38 +0800

    Add minecraft command (WIP)

--- bot.py
+++ bot.py
@@ -31,5 +31,20 @@ class MyClient(discord.Client):
                     constants.WHOIS_CHOICES[random.randint(0, len(constants.WHOIS_CHOICES))]
                 ))
 
+        if command == "$minecraft" or command == "$MINECRAFT":
+            print("Message from {0.author}: {0.content}".format(message))
+
+            # My machine really only has one java process running...
+            minecraft_server_filename = "java"
+
+            if utils.check_if_process_running(minecraft_server_filename):
+                print("Process {} detected".format(minecraft_server_filename))
+                await message.channel.send("Minecraft server is up at {}:25565".format(
+                    utils.get_public_ip()
+                ))
+            else:
+                print("No minecraft server process detected!")
+                await message.channel.send("Minecraft server is offline :(")
+
 client = MyClient()
 client.run(constants.TOKEN)
